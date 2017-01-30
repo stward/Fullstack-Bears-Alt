19a56c5d9699a2fbfa2745c79c683b14016d9650
Spencer Ward <spencertward@gmail.com>
2017-01-30 14:49:10 -0700

    Implemented host method for bears resource

--- server.js
+++ server.js
@@ -1,12 +1,37 @@
 var express = require('express');
 var app = express();
-var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
 var Bear = require('./models/bear');
 
+var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/fullstack-bears');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/api/bears', function(req, res) {
   res.json({message: "Bears route"});
+});
+
+app.post('/api/bears', function(req, res) {
+
+  var bear = new Bear({
+    age: req.body.age,
+    species: req.body.species,
+    name: req.body.name,
+    weight: req.body.weight,
+    location: req.body.location,
+    attitude: req.body.attitude
+  });
+
+  bear.save(function(err, bearData) {
+    if(err) {
+      console.log(err, "error with your bear");
+    } else {
+      res.json(bearData);
+    }
+  });
+
 });
 
 app.listen(3000, function() {
