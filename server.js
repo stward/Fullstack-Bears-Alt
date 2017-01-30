96f21af9a8ae41df0e2b8072b75f3ef6ed0a7ac6
Spencer Ward <spencertward@gmail.com>
2017-01-30 14:19:48 -0700

    Defined mongoose schema for bear, which is the blueprint for the data model.

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var mongoose = require('mongoose');
+var Bear = require('./models/bear');
 
 mongoose.connect('mongodb://localhost/fullstack-bears');
 
