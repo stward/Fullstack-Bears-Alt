0aa4f895ac79cfd6c6f3b963920ee7b25be876d5
Spencer Ward <spencertward@gmail.com>
2017-01-30 11:24:14 -0700

    Added some API endpoints

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 var express = require('express');
 var app = express();
 
+app.get('/', function(req, res) {
+  res.send("monday")
+});
+
+app.get('/api/greeting', function(req, res) {
+  res.send("still monday")
+});
+
 app.listen(3000, function() {
   console.log("Express up and running on port 3000.");
 });
