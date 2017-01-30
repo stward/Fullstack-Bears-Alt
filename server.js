d66f23ff1c84840cb1e7ac24f9253d46d395e83e
Spencer Ward <spencertward@gmail.com>
2017-01-30 11:27:52 -0700

    Added happy greeting and sad greeting.

--- server.js
+++ server.js
@@ -2,11 +2,15 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res) {
-  res.send("monday")
+  res.send("welcome 2 heck.")
 });
 
-app.get('/api/greeting', function(req, res) {
-  res.send("still monday")
+app.get('/api/happy_greeting', function(req, res) {
+  res.send("monday! welcome!")
+});
+
+app.get('/api/sad_greeting', function(req, res) {
+  res.send("still monday. go home.")
 });
 
 app.listen(3000, function() {
