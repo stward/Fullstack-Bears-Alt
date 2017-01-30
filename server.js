d427788502a58acdce61c648ec998ee442e8f638
Spencer Ward <spencertward@gmail.com>
2017-01-30 10:58:17 -0700

    Created server

--- server.js
+++ server.js
@@ -1,1 +1,9 @@
-console.log("Hello world");
+var http = require('http');
+
+http.createServer( function(req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World');
+}).listen(3000);
+
+
+console.log("Server is up and running!");
