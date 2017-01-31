b1aaf1292f13023ceb11117c10ffc84986d6347d
Spencer Ward <spencertward@gmail.com>
2017-01-31 12:05:52 -0700

    Added index and bears EJS views

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var moment = require('moment');
 var Bear = require('./models/bear');
 
 var mongoose = require('mongoose');
@@ -10,6 +11,21 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.set('view engine', 'ejs');
+
+app.get('/', function(req, res) {
+  var date = moment();
+  res.render('index', {name: "Spencer", date: moment().format("MMM Do YYYY"), time: date.format("hh:mm")});
+});
+
+app.get('/bears', function(req, res) {
+  Bear.find(function(err, data) {
+    if(err) {
+      console.log(err, "Error finding bears");
+    } else {
+      res.render('bears', {bears: data});
+    }
+  });
+});
 
 app.get('/api/bears', function(req, res) {
   Bear.find(function(err, data) {
