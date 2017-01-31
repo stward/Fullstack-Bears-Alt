10a8b2a42dadaa9598892b5cfeb52eef05102c91
Spencer Ward <spencertward@gmail.com>
2017-01-31 15:10:11 -0700

    Put a little bear picture background in the jumbotron

--- public/js/main.js
+++ public/js/main.js
@@ -9,13 +9,4 @@ $(document).ready(function() {
       window.location = '/bears';
     });
   });
-  $('#add').on('click', function() {
-    var bearId = $(this).attr('id');
-    $.ajax({
-      url: '/api/bears/' + bearId,
-      method: 'DELETE'
-    }).done(function(data) {
-      console.log("bear deleted");
-    });
-  });
 });
